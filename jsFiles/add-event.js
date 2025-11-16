c55ce79959af8ad552f2ad3cfe9711d9d0b7a463
Nabhil Irtisham <nabhil.irtisham@gmail.com>
2025-11-16 03:14:14 -0500

    Finish add-event.js

--- jsFiles/add-event.js
+++ jsFiles/add-event.js
@@ -4,7 +4,7 @@ Load venues and add events
 
 const apiBase = "http://localhost:3001";                //Backend API base URL
 
-//Lod venus from backend and fill dropdown menu
+//Load venues from backend and fill dropdown menu
 async function loadVenues(){
     const response = await fetch(`${apiBase}/api/events/venues`, {credentials: "include"});     //GET request to backend for venues, sending sessions cookies as well
     const venueData = await response.json()                                 //Convert JSON response to JS object
@@ -17,3 +17,26 @@ async function loadVenues(){
     });
 }
 
+//Send event data to backend to create new event
+document.getElementById("eventForm").addEventListener("submit", async (e) => {      //Add submit event listener to eventForm form
+    e.preventDefault();                                         //Prevent default form submission
+
+    const reqBody = {                                           //Request body: event venue ID, title, description, start/end times, price, and status
+        venue_id: document.getElementById("venue_id").value,
+        title: document.getElementById("title").value,
+        event_description: document.getElementById("event_description").value,
+        start_time: document.getElementById("start_time").value,
+        end_time: document.getElementById("end_time").value,
+        standard_price: document.getElementById("standard_price").value,
+        event_status: document.getElementById("event_status").value
+    };
+
+    await fetch(`${apiBase}/api/events/venues`, {               //Send POST request to backend providing event information, along with session cookies
+        method: "POST",
+        headers: { "Content-Type": "application/json"},
+        credentials: "include",
+        body: JSON.stringify(reqBody)
+    });
+
+    window.location.href="orgDashboard.html";                   //Redirect to dashboard and reload page
+});
